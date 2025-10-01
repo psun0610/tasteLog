963a768401dc18be5cdd4ef4fedea9a4b0ab2572
SunYoung <psy010610@naver.com>
2025-10-01 22:51:28 +0900

    [Feat[ 홈 화면에 트렌드 컴포넌트 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,18 +5,27 @@ import ReviewList from '@/features/review/components/reviewList'
 import { useNavigate } from 'react-router-dom'
 import RecommendRestaurant from '@/features/restaurant/components/recommend'
 import { reviews } from '@/consts/dummy'
+import TrendList from '@/features/restaurant/components/trendList'
 
 const Home = () => {
     const navigate = useNavigate()
 
     return (
         <div id="home">
-            <SectionBox title="친구들이 다녀온 맛집" titleClick={() => navigate('/reviewFeed')}>
-                <ReviewList reviews={reviews} />
-            </SectionBox>
-            <SectionBox title="추천 맛집" titleClick={() => navigate('/recommendRestaurant')}>
-                <RecommendRestaurant />
-            </SectionBox>
+            <div className="home-header">
+                <h1>맛팔</h1>
+            </div>
+            <div className="home-content">
+                <SectionBox title="친구들이 다녀온 맛집" titleClick={() => navigate('/reviewFeed')}>
+                    <ReviewList reviews={reviews} />
+                </SectionBox>
+                <SectionBox title="추천 맛집" titleClick={() => navigate('/recommendRestaurant')}>
+                    <RecommendRestaurant />
+                </SectionBox>
+                <SectionBox title="트렌드 TOP 10" contentBox={false}>
+                    <TrendList />
+                </SectionBox>
+            </div>
             <HomeFloatingButton />
         </div>
     )
