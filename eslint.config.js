cce795483a68e0bb77de84d144eda7734196b243
SunYoung <psy010610@naver.com>
2025-10-18 16:12:51 +0900

    [Init] Eslint 의존성 추가 및 설정 변경경

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,32 @@
-import tsESLintParser from '@typescript-eslint/parser'
+// eslint.config.js
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
 
-export default {
-    parser: tsESLintParser,
-    plugins: ['react', 'react-hooks', '@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        rules: {
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
     },
-    rules: {},
-}
+]
